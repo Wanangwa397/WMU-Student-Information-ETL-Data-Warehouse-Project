52a2b0fe9043d17585b56f3a24ea8dad99400172
Wanangwa Msiska <msiskawanangwa7@gmail.com>
2026-04-27 23:24:00 -0400

    Upload warehouse tables cleanup script
    Added SQL script for clearing warehouse tables before loading clean validated data, using foreign key-safe delete logic to support the full refresh ETL process.

---
+++ SQL Scripts/10_Clear_Warehouse_Tables.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE warehouse.FactStudentAttendance;
+TRUNCATE TABLE warehouse.FactFinancialAid;
+TRUNCATE TABLE warehouse.FactAcademicPerformance;
+TRUNCATE TABLE warehouse.FactEnrollment;
+
+DELETE FROM warehouse.DimStudent;
